4a6d2affa0522b0d68a845e1b0e72a39a37d141a
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-12-15 06:00:29 +0000

    feat: `Counter`コンポーネントを追加

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,28 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "Hello QR Code Generator" }</h1>
+            <Counter />
+        </>
+    }
+}
+
+#[function_component(Counter)]
+fn counter() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+    html! {
+        <div>
+            <button {onclick}>{"+1"}</button>
+            <p>{*counter}</p>
+        </div>
     }
 }
 
